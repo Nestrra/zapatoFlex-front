9d9309de9a2bc942a72575051d9798dc8d9e8164
raul <nestrra@gmail.com>
2026-02-25 20:14:22 -0500

    Configuracion inicial

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       
+      </div>
+      <h1>Muy pronto ZapatoFlex S.A.S.</h1>
+      <div className="card">
+       
+        <p>
+          
+        </p>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
